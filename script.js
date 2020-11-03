c265cefc26a3b8a58dbc991ed88b46f881cbd6d5
Tuyen Nguyen <tuyen.nguyen@hansencx.com>
2020-11-03 23:08:30 +0700

    Add design for list movie

---
+++ script.js
@@ -0,0 +1,67 @@
+const APIKEY = "04c35731a5ee918f014970082a0088b1";
+const APIURL = "https://api.themoviedb.org/3/";
+const API_POP_MOVIES = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1"
+const IMGPATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCHAPI = "https://api.themoviedb.org/3/search/movie?&api_key=04c35731a5ee918f014970082a0088b1&query="
+
+const main = document.querySelector('main');
+const form = document.getElementById('form');
+const search = document.querySelector('.search');
+
+// initially get movies
+getMovies(API_POP_MOVIES);
+
+async function getMovies(url) {
+    const res = await fetch(url);
+    const resData = await res.json();
+
+    showMovies(resData.results);
+}
+
+function showMovies(movies) {
+
+    // clear main
+    main.innerHTML = "";
+
+    movies.forEach(movie => {
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        const { poster_path, title, vote_average, overview } = movie;
+
+        movieEl.innerHTML = `
+        <img src="${IMGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div>
+        <div class="overview">
+            <h3>Overview:</h3>
+            ${overview}
+        </div>
+        `
+
+        main.appendChild(movieEl);
+    });
+}
+
+function getClassByRate(vote) {
+    if (vote > 8) {
+        return 'green';
+    } else if (vote >= 5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const searchTerm = search.value;
+
+    if (searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+        search.value = "";
+    }
+})
+
