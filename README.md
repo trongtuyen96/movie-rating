29eb3d03b1a06c484331fccb0b7ccc44638d57f9
Tuyen Nguyen <tuyen.nguyen@hansencx.com>
2020-11-09 23:47:24 +0700

    Add movie popup info with provided from API, get review and casts

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 An movie app for viewing rating and description of movies built on HTML, CSS and Javascript.
 
 To-do:
-- An view popup to see specific movie follow the design
-- Add budget, genre, language, release date, video, vote_average, runtime, revenue in pop-up form movies/id
-- Add cast from movie/id/credits
-- Add button to see review and fetch from /movie/id/reviews
-- Add navigation bar with lastest, now playing, popular, top rated, upcoming movies
-- Add trding tab from trending/{media_type}/{time_window}
+- [x] An view popup to see specific movie follow the design
+- [x] Add budget, genre, language, release date, video, vote_average, runtime, revenue in pop-up form movies/id
+- [x] Add cast from movie/id/credits
+- [] Add button to see review and fetch from /movie/id/reviews
+- [] Add navigation bar with lastest, now playing, popular, top rated, upcoming movies
+- [] Add trding tab from trending/{media_type}/{time_window}
--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const form = document.getElementById('form');
 const search = document.querySelector('.search');
 
 const moviePopup = document.getElementById('movie-popup');
+const closePopupBtn = document.getElementById('close-popup');
 
 // initially get movies
 getMovies(API_POP_MOVIES);
@@ -86,4 +87,82 @@ async function openMovieDetail(movieId) {
 
     console.log(resData)
     moviePopup.classList.remove('hidden');
+
+    // Specify movie info
+    const coverEl = document.getElementById('cover');
+    const averageEl = document.getElementById('average');
+    const titleEl = document.getElementById('title');
+    const runtimeEl = document.getElementById('runtime');
+    const releaseEl = document.getElementById('release');
+    const genresEl = document.querySelector('.movie-genre');
+    const summaryEl = document.getElementById('summary');
+    const castEl = document.getElementById('cast');
+    const reviewListEl = document.querySelector('.review-list');
+
+    // Cover
+    coverEl.setAttribute('src', getPosterPath(resData.poster_path));
+
+    // Average
+    averageEl.innerHTML = resData.vote_average;
+    if (resData.vote_average > 8) {
+        averageEl.style.backgroundColor = '#008000';
+    } else if (resData.vote_average >= 5) {
+        averageEl.style.backgroundColor = '#ffa500';
+    } else {
+        averageEl.style.backgroundColor = '#ff0000';
+    }
+
+    // Title
+    titleEl.innerHTML = resData.title;
+
+    // Runtime
+    const hour = parseInt(resData.runtime / 60);
+    const min = parseInt(resData.runtime % 60);
+    runtimeEl.innerHTML = `${hour}h ${min}m`;
+
+    // Release
+    releaseEl.innerHTML = resData.release_date;
+
+    // Genres
+    genresEl.innerHTML = ""
+    resData.genres.forEach(element => {
+        const genreEl = document.createElement('span');
+        genreEl.innerHTML = element.name;
+        genresEl.appendChild(genreEl);
+    });
+
+    // Summary
+    summaryEl.innerHTML = resData.overview;
+
+    // Cast
+    const resCredit = await fetch(APIURL + "movie/" + movieId + "/credits" + APIKEY_URL);
+    const resCreditData = await resCredit.json();
+    console.log(resCreditData);
+
+    castEl.innerHTML = "";
+    resCreditData.cast.forEach(element => {
+        const listCastEl = document.createElement('li');
+        listCastEl.innerHTML = `<img src="${getPosterPath(element.profile_path)}" alt="">
+        <span>${element.name}</span>`;
+        castEl.appendChild(listCastEl);
+    });
+
+    // Review
+    const resReview = await fetch(APIURL + "movie/" + movieId + "/reviews" + APIKEY_URL);
+    const resReviewData = await resReview.json();
+    console.log(resReviewData);
+
+    reviewListEl.innerHTML = "";
+    resReviewData.results.forEach(element => {
+        const reviewEl = document.createElement('div');
+        reviewEl.classList.add('review-info');
+        reviewEl.innerHTML = `
+        <div class="review-name">${element.author}</div>
+        <div class="review-text">${marked(element.content)}</div>`;
+        reviewListEl.appendChild(reviewEl);
+    });
+
+    closePopupBtn.addEventListener('click', () => {
+        moviePopup.classList.add('hidden');
+    })
 }
